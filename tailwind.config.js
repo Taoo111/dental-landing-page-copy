6c79bdb3a4bdfa4a866fb934f1778a9371396064
Dawid <dawidjura12@gmail.com>
2022-09-18 16:13:27 +0200

    Tailwind setup + Started building navbar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {},
+    extend: {
+      colors: {
+        darkBlue: "#181945",
+        purple: "#583FBC",
+      },
+    },
+  },
+  plugins: [],
+};
